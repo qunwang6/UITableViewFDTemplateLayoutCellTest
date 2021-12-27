25cdc4956d6c26e0ce0df3d6a01284fca7956ce7
qun <qunwang6@gmail.com>
2021-12-27 10:41:59 +0800

    2

--- tableViewTest/MyTableViewCell.m
+++ tableViewTest/MyTableViewCell.m
@@ -9,9 +9,14 @@
 
 #import "MyTableViewCell.h"
 #import "Masonry.h"
-@implementation MyTableViewCell{
-    UILabel *_textLB;
-}
+#define CFFlexibleWidth(c) (c/375.0*UIScreen.mainScreen.bounds.size.width)
+
+@interface MyTableViewCell ()
+@property (nonatomic, strong) UILabel *textLB;
+@property (nonatomic, strong) UIImageView* goodsImageView;
+
+@end
+@implementation MyTableViewCell
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
@@ -30,7 +35,21 @@
     [self.contentView addSubview:_textLB];
     [_textLB mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.left.equalTo(self.contentView).offset(10);
-        make.bottom.right.equalTo(self.contentView).offset(-10);
+        make.right.equalTo(self.contentView.mas_right).offset(-10);
+        make.height.equalTo(_textLB);
+        //make.bottom.right.equalTo(self.contentView).offset(-100);
+    }];
+    
+    _goodsImageView = [[UIImageView alloc] init];
+    _goodsImageView.image = [UIImage imageNamed:@"default"];
+    _goodsImageView.backgroundColor = [UIColor clearColor];
+    [self.contentView addSubview:_goodsImageView];
+    [_goodsImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(_textLB.mas_bottom).offset(13);
+        make.left.mas_equalTo(CFFlexibleWidth(13));
+        make.width.mas_equalTo(CFFlexibleWidth(50));
+        make.height.mas_equalTo(CFFlexibleWidth(50));
+        make.bottom.equalTo(self.contentView).offset(-10);
     }];
 }
 /**
